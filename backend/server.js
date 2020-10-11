2aa308af808b6c57ee36374b612d30b59b1c2672
Pavan <pavantejakusunuri@gmail.com>
2020-10-11 16:13:53 +0530

    Added dotenv file in project

--- backend/server.js
+++ backend/server.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const dotenv = require("dotenv");
 const products = require('./data/products')
 const app = express()
 
@@ -15,4 +16,6 @@ app.get('/api/products/:id', (req, res) => {
     res.json(product)
 })
 
-app.listen(5000, console.log('Server running on port 5000'))
+const PORT = process.env.PORT || 5000
+
+app.listen(5000, console.log(`Server running on ${process.env.NODE_ENV} port ${PORT}`))
