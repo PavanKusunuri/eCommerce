6b34735a97dcde39dc6c6df0f024064b0b2070f4
Pavan <pavantejakusunuri@gmail.com>
2020-10-28 21:48:52 +0530

    Added Colors in Project

--- backend/server.js
+++ backend/server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const dotenv = require("dotenv");
 const connectDB = require("./config/db.js")
 const products = require('./data/products')
-
+const colors = require('colors')
 dotenv.config()
 
 connectDB()
@@ -25,4 +25,4 @@ app.get('/api/products/:id', (req, res) => {
 
 const PORT = process.env.PORT || 5000
 
-app.listen(5000, console.log(`Server running on ${process.env.NODE_ENV} port ${PORT}`))
+app.listen(5000, console.log(`Server running on ${process.env.NODE_ENV} port ${PORT}`.yellow.bold))
