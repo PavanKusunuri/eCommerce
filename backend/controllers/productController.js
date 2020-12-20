bb2cdaa2b748b54eac7e6e175d73aa201bdc0d5b
Pavan <pavantejakusunuri@gmail.com>
2020-12-20 14:38:33 +0530

    Added Get Product by id method in code

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -8,3 +8,21 @@ const getProducts = asyncHandler(async (req, res) => {
     const products = await Product.find({})
     res.json(products)
 })
+
+//  @description Fetch Single Product
+//  @route           GET/api/products/:id
+//  @access         Public
+
+const getProductById = asyncHandler(async (req, res) => {
+    const produt = await Product.findById(req.params.id)
+    if (produt) {
+        res.json(product)
+    } else {
+        res.status(404)
+        throw new Error('Product not Found!!')
+    }
+})
+
+export {
+    getProductById, getProducts
+}
