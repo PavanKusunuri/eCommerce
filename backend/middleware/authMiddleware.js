7a9f963ad7f7b1f1d96624b6d1a2f2fd9a5a6a68
Pavan <pavantejakusunuri@gmail.com>
2020-12-22 06:08:45 +0530

    Added Authorization middleware in project

---
+++ backend/middleware/authMiddleware.js
@@ -0,0 +1,32 @@
+import jwt from 'jsonwebtoken';
+import asyncHandler from 'express-async-handler'
+import User from '../models/userModel.js';
+
+const protect = asyncHandler(async (req, res, next) => {
+    let token;
+
+    if (
+        req.headers.authorization &&
+        req.headers.authorization.startsWith('Bearer')) {
+        token = req.headers.authorization.split(" ")[1];
+    }
+
+    try {
+        const decoded = jwt.verify(token, process.env.JWT_SECRET)
+        console.log(decoded.id)
+        req.user = await User.findById(decoded.id);
+        next()
+    } catch (err) {
+        console.error(err)
+        res.status(401)
+        throw new Error('Not authorized, new ')
+    }
+
+
+    if (!token) {
+        res.status(401)
+        throw new Error('Not Authorized, no token')
+    }
+})
+
+export { protect }
--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -1,11 +1,13 @@
 import express from 'express'
 const router = express.Router()
 import {
-    authUser
+    authUser, getUserProfile
 } from '../controllers/userController.js'
+import { protect } from '../middleware/authMiddleware.js';
+
 
 router.post("/login", authUser);
-
+router.route('/profile').get(protect, getUserProfile)
 // router.route('/login').post(authUser)
 
 export default router
