1f651e8dae166b1bc3ab0f1a6f728dc7d9daf11a
Pavan <pavantejakusunuri@gmail.com>
2020-12-25 19:59:40 +0530

    axios spell is corrected

--- frontend/src/actions/userActions.js
+++ frontend/src/actions/userActions.js
@@ -1,4 +1,4 @@
-import Axios from "axios"
+import axios from "axios"
 import { USER_LOGIN_FAIL, USER_LOGIN_REQUEST, USER_LOGIN_SUCCESS } from "../constants/userConstants"
 
 export const login = (email, password) => async (dispatch) => {
