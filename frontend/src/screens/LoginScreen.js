7c29ae4db6f33ce7df3cede165c238bef6e3bafd
Pavan <pavantejakusunuri@gmail.com>
2020-12-28 08:02:50 +0530

    Issues with login functionality is fixed

--- frontend/src/screens/LoginScreen.js
+++ frontend/src/screens/LoginScreen.js
@@ -1,10 +1,10 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { Link } from 'react-router-dom';
 import { Form, Button, Row, Col } from 'react-bootstrap';
 import { useDispatch, useSelector } from 'react-redux';
 import Message from '../components/Message';
 import Loader from '../components/Loader';
-import { Login } from '../actions/userActions';
+import { login } from '../actions/userActions';
 import FormContainer from '../components/FormContainer';
 
 const LoginScreen = ({ location, history }) => {
