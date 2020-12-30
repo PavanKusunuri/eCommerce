3ae1874dd9d0861ed09dd73e8411511ab8376bcf
Pavan <pavantejakusunuri@gmail.com>
2020-12-30 15:03:45 +0530

    Added Placeorder screen in project

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -12,6 +12,7 @@ import RegisterScreen from './screens/RegisterScreen';
 import ProfileScreen from './screens/ProfileScreen';
 import { ShippingScreen } from './screens/ShippingScreen';
 import { PaymentScreen } from './screens/PaymentScreen';
+import PlaceOrderScreen from './screens/PlaceOrderScreen'
 
 const App = () => {
   return (
@@ -19,7 +20,9 @@ const App = () => {
       <Header />
       <main className='py-3'>
         <Container>
-          <Route path="/payment" component={PaymentScreen}
+          <Route path="/payment" component={PaymentScreen} />
+          <Route path="/placeorder" component={PlaceOrderScreen} />
+
           <Route path="/shipping" component={ShippingScreen} />
           <Route path='/login' component={LoginScreen} />
           <Route path='/register' component={RegisterScreen} />
