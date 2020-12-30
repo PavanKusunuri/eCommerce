eee1043d81527d3399d7e0032baaf084ac6fa418
Pavan <pavantejakusunuri@gmail.com>
2020-12-30 15:15:47 +0530

    Added order Controller, order Routes in project

--- backend/server.js
+++ backend/server.js
@@ -7,6 +7,7 @@ import { notFound, errorHandler } from './middleware/errorMiddleware.js'
 import connectDB from "./config/db.js"
 import productRoutes from "./routes/productRoutes.js"
 import userRoutes from './routes/userRoutes.js';
+import orderRoutes from './routes/orderRoutes.js';
 
 dotenv.config()
 
@@ -18,18 +19,12 @@ app.use(cors())
 
 // Body parser
 app.use(express.json());
-// app.use(express.json())
 
 app.use('/api/products', productRoutes)
 app.use('/api/users', userRoutes)
+app.use('/api/orders', orderRoutes)
 
 
-
-// app.use(function (req, res, next) {
-//     res.header("Access-Control-Allow-Origin": *); // update to match the domain you will make the request from
-//     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-//     next();
-// });
 
 
 app.use(notFound)
