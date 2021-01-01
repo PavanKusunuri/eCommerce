5e0dd573d9f73bf07038dba9d6f5ca54b68e9243
Pavan <pavantejakusunuri@gmail.com>
2021-01-01 12:20:27 +0530

    Added Controller method to update the paypal payment

--- backend/controllers/orderController.js
+++ backend/controllers/orderController.js
@@ -52,4 +52,29 @@ const getOrderById = asyncHandler(async (req, res) => {
 })
 
 
-export { addOrderItems, getOrderById }
+//  @desc Update order to paid
+//  @route /api/orders/:id/pay
+//  @access private
+
+const updateOrderToPaid = asyncHandler(async (req, res) => {
+    const order = await (await Order.findById(req.params.id))
+
+    if (order) {
+        order.isPaid = true
+        order.paidAt = Date.now()
+        order.paymentResult = {
+            id: req.body.id,
+            status: req.body.status,
+            update_time: req.body.update_item,
+            email_address: req.body.payer.email_address
+        }
+        const updatedOrder = await order.save()
+        res.json = updatedOrder
+    } else {
+        throw new Error('Order not found ..')
+    }
+})
+
+
+
+export { addOrderItems, getOrderById, updateOrderToPaid }
