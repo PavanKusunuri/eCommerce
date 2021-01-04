8db4eb98a71dfc4577c31cf23b6ce4099c7e0b0d
Pavan <pavantejakusunuri@gmail.com>
2021-01-04 21:31:28 +0530

    UserUpdate file import is added in code

--- frontend/src/actions/userActions.js
+++ frontend/src/actions/userActions.js
@@ -22,7 +22,8 @@ import {
     USER_DELETE_FAIL,
     USER_DELETE_SUCCESS,
     USER_UPDATE_REQUEST,
-    USER_UPDATE_SUCCESS
+    USER_UPDATE_SUCCESS,
+    USER_UPDATE_FAIL
 } from "../constants/userConstants"
 import { ORDER_LIST_MY_RESET } from '../constants/orderConstants'
 
