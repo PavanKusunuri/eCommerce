9cf1457b74ef3e3caf9a6cfb4bfe6139093c7ffc
Pavan <pavantejakusunuri@gmail.com>
2021-01-04 21:30:31 +0530

    userUpdate Fixes in code

--- frontend/src/screens/userEditScreen.jsx
+++ frontend/src/screens/userEditScreen.jsx
@@ -4,7 +4,9 @@ import { Form, Button } from 'react-bootstrap';
 import { useDispatch, useSelector } from 'react-redux';
 import Message from '../components/Message';
 import Loader from '../components/Loader';
-import { getUserDetails } from '../actions/userActions';
+import { getUserDetails, updateUser } from '../actions/userActions';
+import { USER_UPDATE_RESET} from '../constants/userConstants';
+
 import FormContainer from '../components/FormContainer';
 
 const UserEditScreen = ({ match, history }) => {
@@ -15,22 +17,31 @@ const userId =match.params.id
     const dispatch = useDispatch()
 
     const userDetails = useSelector(state => state.userRegister)
+const userUpdate = useSelector((state)=> state.userUpdate)
+const { loading: loadingUpdate, error: errorUpdate, success: successUpdate} = userUpdate;
 
     const { loading, error, user } = userDetails;
 
 
     useEffect(() => {
-     if(!user.name || user._id !== userId ) {
-dispatch(getUserDetails(userId))
-     } else {
-         setName(user.name)
-         setEmail(user.email)
-         setIsAdmin(user.isAdmin)
-     }
-    }, [dispatch, userId, user])
+        if(successUpdate) {
+            dispatch({ type: USER_UPDATE_RESET})
+            history.push('/admin/userlist')
+        } else {
+            if(!user.name || user._id !== userId ) {
+                dispatch(getUserDetails(userId))
+                     } else {
+                         setName(user.name)
+                         setEmail(user.email)
+                         setIsAdmin(user.isAdmin)
+                     }
+        }
+     
+    }, [dispatch, userId, user, history, successUpdate])
 
     const submitHandler = (e) => {
         e.preventDefault()
+        dispatch({ _id: userId, name, email, isAdmin })
     }
 
     return (
@@ -40,6 +51,8 @@ dispatch(getUserDetails(userId))
         </Link>
         <FormContainer>
             <h1> Edit User</h1>
+            {loadingUpdate && <Loader />}
+            {errorUpdate && <Message variant='danger'>{errorUpdate}</Message>}
             { loading ? <Loader /> : error ? (<Message variant='danger'>{error}</Message>)
             :(
                 <Form onSubmit={submitHandler}>
