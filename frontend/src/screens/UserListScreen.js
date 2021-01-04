c3bed016ab0ec8fabb3b22b18541501a6e1dc7bb
Pavan <pavantejakusunuri@gmail.com>
2021-01-04 20:54:56 +0530

    lint error fixes

--- frontend/src/screens/UserListScreen.js
+++ frontend/src/screens/UserListScreen.js
@@ -24,7 +24,7 @@ const UserListScreen = ({ history }) => {
         } else {
             history.pushState('/login')
         }
-    }, [dispatch, history, successDelete])
+    }, [dispatch, history, successDelete, userInfo])
 
     const deleteHandler = (id) => {
         if (window.confirm('Are you sure')) {
@@ -60,7 +60,7 @@ const UserListScreen = ({ history }) => {
                                             <i className="fas fa-times" style={{ color: 'red' }}></i>
                                         )}</td>
                                         <td>
-                                            <LinkContainer to={`/user/${user._id}/edit`}>
+                                            <LinkContainer to={`/admin/user/${user._id}/edit`}>
                                                 <Button variant='light' className='btn-sm'>
                                                     <i className='fas fa-edit'>
 
