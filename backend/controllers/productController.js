caaa8b5cbcc59b91defce4c3a87dc5be694c0e9e
Pavan <pavantejakusunuri@gmail.com>
2021-01-05 22:13:19 +0530

    Added Product removal functionality in code

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -25,6 +25,21 @@ const getProductById = asyncHandler(async (req, res) => {
     }
 })
 
+//  @description  DELETE a product
+//  @route           DELETE /api/products/:id
+//  @access         Private/Admin
+
+const deleteProduct = asyncHandler(async (req, res) => {
+    const product = await Product.findById(req.params.id)
+    if (product) {
+        await product.remove()
+        res.json({ message: 'Product Removed' })
+    } else {
+        res.status(404)
+        throw new Error('Product not Found!!')
+    }
+})
+
 export {
     getProductById, getProducts
 }
