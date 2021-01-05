3fdf41fb4776f5c2a17cc82dfe47a9cfdab04d60
Pavan <pavantejakusunuri@gmail.com>
2021-01-05 22:24:36 +0530

    Added Product Action in code

--- frontend/src/actions/productActions.js
+++ frontend/src/actions/productActions.js
@@ -5,7 +5,10 @@ import {
     PRODUCT_LIST_FAIL,
     PRODUCT_DETAILS_REQUEST,
     PRODUCT_DETAILS_SUCCESS,
-    PRODUCT_DETAILS_FAIL
+    PRODUCT_DETAILS_FAIL,
+    PRODUCT_DELETE_REQUEST,
+    PRODUCT_DELETE_SUCCESS,
+    PRODUCT_DELETE_FAIL
 } from '../constants/productsConstants'
 
 export const listProducts = () => async (dispatch) => {
@@ -37,6 +40,30 @@ export const listProductDetails = (id) => async (dispatch) => {
     } catch (error) {
         dispatch({
             type: PRODUCT_DETAILS_FAIL,
+            payload: error.response && error.response.data.message ?
+                error.response.data.message : error.response
+        })
+    }
+}
+
+
+export const deleteProduct = (id) => async (dispatch) => {
+    try {
+        dispatch({ type: PRODUCT_DELETE_REQUEST })
+        const { userLogin: { userInfo }, } = getState()
+        const config = {
+            headers: {
+                Authorization: `Bearer ${userInfo.token}`,
+            },
+        }
+
+        await axios.delete(`http://localhost:5000/api/products/${id}`, config)
+        dispatch({
+            type: PRODUCT_DELETE_SUCCESS,
+        })
+    } catch (error) {
+        dispatch({
+            type: PRODUCT_DELETE_FAIL,
             payload: error.response && error.response.data.message ?
                 error.response.data.message : error.response
         })
