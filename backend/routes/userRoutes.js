2f88d92c4c33b811aac35d54bbe0c8763bc456cd
Pavan <pavantejakusunuri@gmail.com>
2021-01-06 19:32:22 +0530

    Added uploadRoutes a new file to manage file uploads in node

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -1,15 +1,19 @@
 import express from 'express'
 const router = express.Router()
 import {
-    authUser, deleteUser, getUserProfile, getUserById, updateUser, getUsers, registerUser, updateUserProfile
+    authUser,
+    registerUser,
+    getUserProfile,
+    updateUserProfile,
+    getUsers,
+    deleteUser,
+    getUserById,
+    updateUser,
 } from '../controllers/userController.js'
-import { protect, admin } from '../middleware/authMiddleware.js';
+import { protect, admin } from '../middleware/authMiddleware.js'
 
-router
-    .route('/')
-    .post(registerUser)
-    .get(protect, admin, getUsers);
-router.post("/login", authUser);
+router.route('/').post(registerUser).get(protect, admin, getUsers)
+router.post('/login', authUser)
 router
     .route('/profile')
     .get(protect, getUserProfile)
