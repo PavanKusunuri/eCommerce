51863a950c4895eb416e07c1d1c70b2a4869dbe5
Pavan <pavantejakusunuri@gmail.com>
2021-01-10 15:40:11 +0530

    Updated Reducer files in project

--- frontend/src/reducers/userReducer.js
+++ frontend/src/reducers/userReducer.js
@@ -1,30 +1,30 @@
 import {
-    USER_LOGIN_FAIL,
-    USER_LOGIN_SUCCESS,
-    USER_LOGIN_REQUEST,
-    USER_LOGOUT,
-    USER_REGISTER_FAIL,
-    USER_REGISTER_REQUEST,
-    USER_REGISTER_SUCCESS,
-    USER_DETAIL_FAIL,
-    USER_DETAIL_SUCCESS,
-    USER_DETAIL_REQUEST,
-    USER_UPDATE_PROFILE_FAIL,
-    USER_UPDATE_PROFILE_RESET,
-    USER_UPDATE_PROFILE_SUCCESS,
-    USER_UPDATE_PROFILE_REQUEST,
-    USER_DETAIL_RESET,
+    USER_DETAILS_FAIL,
+    USER_DETAILS_REQUEST,
+    USER_DETAILS_RESET,
+    USER_DETAILS_SUCCESS,
     USER_LIST_REQUEST,
     USER_LIST_SUCCESS,
     USER_LIST_FAIL,
     USER_LIST_RESET,
+    USER_LOGIN_FAIL,
+    USER_LOGIN_REQUEST,
+    USER_LOGIN_SUCCESS,
+    USER_LOGOUT,
+    USER_REGISTER_FAIL,
+    USER_REGISTER_REQUEST,
+    USER_REGISTER_SUCCESS,
+    USER_UPDATE_PROFILE_FAIL,
+    USER_UPDATE_PROFILE_REQUEST,
+    USER_UPDATE_PROFILE_SUCCESS,
     USER_DELETE_REQUEST,
-    USER_DELETE_FAIL,
     USER_DELETE_SUCCESS,
+    USER_DELETE_FAIL,
+    USER_UPDATE_RESET,
     USER_UPDATE_REQUEST,
     USER_UPDATE_SUCCESS,
     USER_UPDATE_FAIL,
-    USER_UPDATE_RESET
+    USER_UPDATE_PROFILE_RESET,
 } from '../constants/userConstants'
 
 export const userLoginReducer = (state = {}, action) => {
@@ -50,6 +50,8 @@ export const userRegisterReducer = (state = {}, action) => {
             return { loading: false, userInfo: action.payload }
         case USER_REGISTER_FAIL:
             return { loading: false, error: action.payload }
+        case USER_LOGOUT:
+            return {}
         default:
             return state
     }
@@ -57,22 +59,18 @@ export const userRegisterReducer = (state = {}, action) => {
 
 export const userDetailsReducer = (state = { user: {} }, action) => {
     switch (action.type) {
-        case USER_DETAIL_REQUEST:
-            return { loading: true }
-        case USER_DETAIL_SUCCESS:
+        case USER_DETAILS_REQUEST:
+            return { ...state, loading: true }
+        case USER_DETAILS_SUCCESS:
             return { loading: false, user: action.payload }
-        case USER_DETAIL_FAIL:
+        case USER_DETAILS_FAIL:
             return { loading: false, error: action.payload }
-
-        case USER_DETAIL_RESET:
-            return {
-                user: {}
-            }
+        case USER_DETAILS_RESET:
+            return { user: {} }
         default:
             return state
     }
 }
-
 
 export const userUpdateProfileReducer = (state = {}, action) => {
     switch (action.type) {
@@ -88,7 +86,6 @@ export const userUpdateProfileReducer = (state = {}, action) => {
             return state
     }
 }
-
 
 export const userListReducer = (state = { users: [] }, action) => {
     switch (action.type) {
@@ -110,7 +107,7 @@ export const userDeleteReducer = (state = {}, action) => {
         case USER_DELETE_REQUEST:
             return { loading: true }
         case USER_DELETE_SUCCESS:
-            return { loading: false, success: true, }
+            return { loading: false, success: true }
         case USER_DELETE_FAIL:
             return { loading: false, error: action.payload }
         default:
@@ -123,15 +120,13 @@ export const userUpdateReducer = (state = { user: {} }, action) => {
         case USER_UPDATE_REQUEST:
             return { loading: true }
         case USER_UPDATE_SUCCESS:
-            return { loading: false, success: true, }
+            return { loading: false, success: true }
         case USER_UPDATE_FAIL:
             return { loading: false, error: action.payload }
-
         case USER_UPDATE_RESET:
             return {
-                user: {}
+                user: {},
             }
-
         default:
             return state
     }
