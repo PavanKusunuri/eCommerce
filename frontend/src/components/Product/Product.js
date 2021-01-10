de0b1d73725f6e73469075bef704389fc584a380
Pavan <pavantejakusunuri@gmail.com>
2021-01-10 16:05:44 +0530

    Updated Common components in code

--- frontend/src/components/Product/Product.js
+++ frontend/src/components/Product/Product.js
@@ -1,7 +1,7 @@
 import React from 'react'
-import { Link } from 'react-router-dom';
-import { Card } from 'react-bootstrap';
-import Rating from '../Rating/Rating';
+import { Link } from 'react-router-dom'
+import { Card } from 'react-bootstrap'
+import Rating from './Rating'
 
 const Product = ({ product }) => {
     return (
@@ -9,6 +9,7 @@ const Product = ({ product }) => {
             <Link to={`/product/${product._id}`}>
                 <Card.Img src={product.image} variant='top' />
             </Link>
+
             <Card.Body>
                 <Link to={`/product/${product._id}`}>
                     <Card.Title as='div'>
@@ -17,12 +18,13 @@ const Product = ({ product }) => {
                 </Link>
 
                 <Card.Text as='div'>
-                    <Rating value={product.rating} text={`${product.numReviews} reviews`} />
+                    <Rating
+                        value={product.rating}
+                        text={`${product.numReviews} reviews`}
+                    />
                 </Card.Text>
 
-                <Card.Text as='h3'>
-                    ${product.price}
-                </Card.Text>
+                <Card.Text as='h3'>${product.price}</Card.Text>
             </Card.Body>
         </Card>
     )
