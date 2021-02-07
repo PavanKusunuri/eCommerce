e178456094bc8d16b12bdf6b419a7f7bebed1904
PavanKusunuri <pavantejakusunuri@gmail.com>
2021-02-07 23:00:03 +0530

    Modified controllers in code

--- backend/controllers/orderController.js
+++ backend/controllers/orderController.js
@@ -1,5 +1,5 @@
-import asyncHandler from 'express-async-handler';
-import Order from '../models/orderModel.js';
+import asyncHandler from 'express-async-handler'
+import Order from '../models/orderModel.js'
 
 // @desc    Create new order
 // @route   POST /api/orders
@@ -30,33 +30,35 @@ const addOrderItems = asyncHandler(async (req, res) => {
             shippingPrice,
             totalPrice,
         })
+
         const createdOrder = await order.save()
+
         res.status(201).json(createdOrder)
     }
 })
 
-//  @desc Get Order by Id
-//  @route /api/orders/:id
-//  @access private
-
+// @desc    Get order by ID
+// @route   GET /api/orders/:id
+// @access  Private
 const getOrderById = asyncHandler(async (req, res) => {
     const order = await Order.findById(req.params.id).populate(
-        'user', 'name', 'email')
+        'user',
+        'name email'
+    )
 
     if (order) {
         res.json(order)
     } else {
-        throw new Error('Order not found ..')
+        res.status(404)
+        throw new Error('Order not found')
     }
 })
 
-
-//  @desc Update order to paid
-//  @route /api/orders/:id/pay
-//  @access private
-
+// @desc    Update order to paid
+// @route   GET /api/orders/:id/pay
+// @access  Private
 const updateOrderToPaid = asyncHandler(async (req, res) => {
-    const order = await (await Order.findById(req.params.id))
+    const order = await Order.findById(req.params.id)
 
     if (order) {
         order.isPaid = true
@@ -64,13 +66,13 @@ const updateOrderToPaid = asyncHandler(async (req, res) => {
         order.paymentResult = {
             id: req.body.id,
             status: req.body.status,
-            update_time: req.body.update_item,
-            email_address: req.body.payer.email_address
+            update_time: req.body.update_time,
+            email_address: req.body.payer.email_address,
         }
 
         const updatedOrder = await order.save()
 
-        res.json = updatedOrder
+        res.json(updatedOrder)
     } else {
         res.status(404)
         throw new Error('Order not found')
@@ -96,17 +98,12 @@ const updateOrderToDelivered = asyncHandler(async (req, res) => {
     }
 })
 
-//  @desc Get logged in user orders
-//  @route /api/orders/myorders
-//  @access private
-
+// @desc    Get logged in user orders
+// @route   GET /api/orders/myorders
+// @access  Private
 const getMyOrders = asyncHandler(async (req, res) => {
-    console.log("get My Orders is called")
-    console.log(req.user._id)
     const orders = await Order.find({ user: req.user._id })
-    console.log(orders)
     res.json(orders)
-    console.log(orders)
 })
 
 // @desc    Get all orders
--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,14 +1,14 @@
-import asyncHandler from 'express-async-handler';
-import User from '../models/userModel.js';
-import generateToken from '../utils/generateToken.js';
+import asyncHandler from 'express-async-handler'
+import generateToken from '../utils/generateToken.js'
+import User from '../models/userModel.js'
 
-//  @desc      Auth user & get token
-//  @route     POST  /api/users/login
-//  @access    Public
-
+// @desc    Auth user & get token
+// @route   POST /api/users/login
+// @access  Public
 const authUser = asyncHandler(async (req, res) => {
     const { email, password } = req.body
-    const user = await User.findOne({ email: email })
+
+    const user = await User.findOne({ email })
 
     if (user && (await user.matchPassword(password))) {
         res.json({
@@ -24,42 +24,45 @@ const authUser = asyncHandler(async (req, res) => {
     }
 })
 
-//  @desc      Auth user & get token
-//  @route     POST  /api/users/login
-//  @access    Public
-
+// @desc    Register a new user
+// @route   POST /api/users
+// @access  Public
 const registerUser = asyncHandler(async (req, res) => {
     const { name, email, password } = req.body
-    const userExists = await User.findOne({ email: email })
+
+    const userExists = await User.findOne({ email })
+
     if (userExists) {
         res.status(400)
-        throw new Error('User already Exists')
+        throw new Error('User already exists')
     }
+
     const user = await User.create({
-        name, email, password
+        name,
+        email,
+        password,
     })
+
     if (user) {
         res.status(201).json({
             _id: user._id,
             name: user.name,
             email: user.email,
             isAdmin: user.isAdmin,
-            token: generateToken(user._id)
+            token: generateToken(user._id),
         })
     } else {
         res.status(400)
-        throw new Error('Invalid User Data')
+        throw new Error('Invalid user data')
     }
-
 })
 
-
-//  @desc      Get User Profile
-//  @route     POST  /api/users/profile
-//  @access    Private
-
+// @desc    Get user profile
+// @route   GET /api/users/profile
+// @access  Private
 const getUserProfile = asyncHandler(async (req, res) => {
     const user = await User.findById(req.user._id)
+
     if (user) {
         res.json({
             _id: user._id,
@@ -67,21 +70,18 @@ const getUserProfile = asyncHandler(async (req, res) => {
             email: user.email,
             isAdmin: user.isAdmin,
         })
-
     } else {
         res.status(404)
         throw new Error('User not found')
     }
 })
 
-
-
-//  @desc      Update User Profile
-//  @route     PUT  /api/users/profile
-//  @access    Private
-
+// @desc    Update user profile
+// @route   PUT /api/users/profile
+// @access  Private
 const updateUserProfile = asyncHandler(async (req, res) => {
     const user = await User.findById(req.user._id)
+
     if (user) {
         user.name = req.body.name || user.name
         user.email = req.body.email || user.email
@@ -90,38 +90,34 @@ const updateUserProfile = asyncHandler(async (req, res) => {
         }
 
         const updatedUser = await user.save()
+
         res.json({
             _id: updatedUser._id,
             name: updatedUser.name,
             email: updatedUser.email,
             isAdmin: updatedUser.isAdmin,
-            token: generateToken(updatedUser._id)
+            token: generateToken(updatedUser._id),
         })
-
     } else {
         res.status(404)
         throw new Error('User not found')
     }
 })
 
-
-//  @desc      Get All Users
-//  @route     POST  /api/users/profile
-//  @access    Private/Admin
-
+// @desc    Get all users
+// @route   GET /api/users
+// @access  Private/Admin
 const getUsers = asyncHandler(async (req, res) => {
     const users = await User.find({})
     res.json(users)
-
 })
 
-
-//  @desc      Delete a user
-//  @route     DELETE  /api/users/:id
-//  @access    Private/Admin
-
+// @desc    Delete user
+// @route   DELETE /api/users/:id
+// @access  Private/Admin
 const deleteUser = asyncHandler(async (req, res) => {
     const user = await User.findById(req.params.id)
+
     if (user) {
         await user.remove()
         res.json({ message: 'User removed' })
@@ -131,12 +127,12 @@ const deleteUser = asyncHandler(async (req, res) => {
     }
 })
 
-//  @desc  GET user by Id
-//   @route  GET/api/users/:id
-//  @access Private/admin
-
+// @desc    Get user by ID
+// @route   GET /api/users/:id
+// @access  Private/Admin
 const getUserById = asyncHandler(async (req, res) => {
     const user = await User.findById(req.params.id).select('-password')
+
     if (user) {
         res.json(user)
     } else {
@@ -145,15 +141,16 @@ const getUserById = asyncHandler(async (req, res) => {
     }
 })
 
-//  @desc     Update user
-//   @route   PUT  /api/users/:id
-//   @access Private/Admin
+// @desc    Update user
+// @route   PUT /api/users/:id
+// @access  Private/Admin
 const updateUser = asyncHandler(async (req, res) => {
     const user = await User.findById(req.params.id)
+
     if (user) {
         user.name = req.body.name || user.name
         user.email = req.body.email || user.email
-        user.isAdmin = req.body.isAdmin || user.isAdmin
+        user.isAdmin = req.body.isAdmin
 
         const updatedUser = await user.save()
 
@@ -161,13 +158,21 @@ const updateUser = asyncHandler(async (req, res) => {
             _id: updatedUser._id,
             name: updatedUser.name,
             email: updatedUser.email,
-            isAdmin: updatedUser.isAdmin
+            isAdmin: updatedUser.isAdmin,
         })
     } else {
         res.status(404)
         throw new Error('User not found')
     }
-
 })
 
-export { authUser, registerUser, getUserProfile, updateUserProfile, getUsers, deleteUser, getUserById, updateUser }
+export {
+    authUser,
+    registerUser,
+    getUserProfile,
+    updateUserProfile,
+    getUsers,
+    deleteUser,
+    getUserById,
+    updateUser,
+}
