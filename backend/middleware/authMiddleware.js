baddc7eaa9c5ecdf3581a06f98e3e0d23434f2ae
PavanKusunuri <pavantejakusunuri@gmail.com>
2021-02-07 23:03:30 +0530

    Modified to auth middle ware

--- backend/middleware/authMiddleware.js
+++ backend/middleware/authMiddleware.js
@@ -1,30 +1,32 @@
-import jwt from 'jsonwebtoken';
+import jwt from 'jsonwebtoken'
 import asyncHandler from 'express-async-handler'
-import User from '../models/userModel.js';
+import User from '../models/userModel.js'
 
 const protect = asyncHandler(async (req, res, next) => {
-    let token;
+    let token
 
     if (
         req.headers.authorization &&
-        req.headers.authorization.startsWith('Bearer')) {
-        token = req.headers.authorization.split(" ")[1];
-    }
+        req.headers.authorization.startsWith('Bearer')
+    ) {
+        try {
+            token = req.headers.authorization.split(' ')[1]
 
-    try {
-        const decoded = jwt.verify(token, process.env.JWT_SECRET)
-        console.log(decoded.id)
-        req.user = await User.findById(decoded.id);
-        next()
-    } catch (err) {
-        console.error(err)
-        res.status(401)
-        throw new Error('Not authorized, new ')
+            const decoded = jwt.verify(token, process.env.JWT_SECRET)
+
+            req.user = await User.findById(decoded.id).select('-password')
+
+            next()
+        } catch (error) {
+            console.error(error)
+            res.status(401)
+            throw new Error('Not authorized, token failed')
+        }
     }
 
     if (!token) {
         res.status(401)
-        throw new Error('Not Authorized, no token')
+        throw new Error('Not authorized, no token')
     }
 })
 
@@ -33,7 +35,7 @@ const admin = (req, res, next) => {
         next()
     } else {
         res.status(401)
-        throw new Error('Not authorized as an Admin')
+        throw new Error('Not authorized as an admin')
     }
 }
 
